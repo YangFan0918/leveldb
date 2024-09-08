17259378a050b88fecaf866f01328a681131052d
YangFan0918 <1539789215@qq.com>
2024-09-08 22:33:57 +0800

    111

--- db/version_edit.h
+++ db/version_edit.h
@@ -96,6 +96,8 @@ class VersionEdit {
   bool has_next_file_number_;
   bool has_last_sequence_;
 
+
+
   std::vector<std::pair<int, InternalKey>> compact_pointers_;
   DeletedFileSet deleted_files_;
   std::vector<std::pair<int, FileMetaData>> new_files_;
